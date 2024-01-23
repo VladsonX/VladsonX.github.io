2c2dba343f018afc9f9378762164a0e147c07b6d
Vlad <vladnovik123456789@gmail.com>
2024-01-23 20:10:59 +0100

    google translate added, photo view added

--- script2.js
+++ script2.js
@@ -5,3 +5,17 @@ btnMenu.addEventListener('click', function () {
    btnBars.classList.toggle("menu-open");
    nav.classList.toggle("nav-open");
 });
+
+var figure = document.querySelectorAll("figure");
+var historyText = document.querySelectorAll(".history_text");
+for (let i = 0; i < figure.length; i++) {
+    const counter = figure[i];
+    const counterText = historyText[i];
+    counter.addEventListener('click', function () {
+        this.classList.toggle("photo_click");
+        this.querySelector("img").classList.toggle("width_disable")
+        counterText.classList.toggle("no-blur");
+
+    });
+}
+
